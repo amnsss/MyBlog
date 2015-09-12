a146436132858628896dac5c29246d79bf8dba4d
Jason Zhang <jason.zhang@meginfo.com>
2015-09-12 18:10:38 +0800

    none

--- js/app.js
+++ js/app.js
@@ -34,21 +34,8 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
                     templateUrl: 'tpls/home.html'
                 },
                 'main@index': {
-                    templateUrl: 'tpls/loginForm.html'
-                }
-            }
-        })
-        .state('booklist', {
-            //url: '/{bookType:[0-9]{1,4}}',
-            views: { //注意这里的写法，当一个页面上带有多个ui-view的时候如何进行命名和视图模板的加载动作
-                '': {
-                    templateUrl: 'tpls/bookList.html'
-                },
-                'booktype@booklist': {
-                    templateUrl: 'tpls/bookType.html'
-                },
-                'bookgrid@booklist': {
-                    templateUrl: 'tpls/bookGrid.html'
+                    templateUrl: 'tpls/loginForm.html',
+                    controller: 'loginCtrl'
                 }
             }
         })
@@ -61,7 +48,7 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
             templateUrl: 'tpls/bookDetail.html'
         })
         .state('userBlog', {
-            url: '/{bookType:[0-9]{1,4}}',
+            url: '/userBlog',
             views: { 
                 '': {
                     templateUrl: 'tpls/userBlog.html'
@@ -77,4 +64,27 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
                 }
             }
         })
+        .state('edit', {
+            url: '/edit',
+            templateUrl: 'tpls/edit.html'
+        })
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,18 @@
 var BlogListModule = angular.module('BlogListModule', []);
 var BlogContentModule = angular.module('BlogContentModule', []);
+
+routerApp.controller('loginCtrl', function($scope, $http){
+	$scope.login = function() {
+		$http.get('../data/blogs/' + 'first' + '.html')
+  			.success(function(result) {
+  			    $element.html(result);
+  			});
+	}
+});
+
 BlogContentModule.controller('blogContentCtrl', function($scope, $http, $state, $stateParams) {
 	
 	
 });
+
+BlogListModule.controller('blogListCtrl')
