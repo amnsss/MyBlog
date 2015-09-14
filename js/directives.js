a31c328256262bdad9821a5763f71f84e19f6f14
unknown <zwj@gmail.com>
2015-09-14 08:21:21 +0800

    none

--- js/directives.js
+++ js/directives.js
@@ -6,8 +6,18 @@ BlogDirectiveModule.directive( 'blogContent', function ($http) {
     link: function ( $scope, $element ) {
       $http.get('../data/blogs/' + 'first' + '.html')
   			.success(function(result) {
-  			    $element.html(result);
+  			  $element.html(result);
   			});
+    }
+  };
+} );
+
+BlogDirectiveModule.directive( 'preview', function () {
+  return {
+    link: function (  scope, element, attr ) {
+      scope.$watch(attr.preview, function(content) {
+        element.html(content);
+      });
     }
   };
 } );
